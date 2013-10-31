694ac048a662b372c71ea55ed30f83dbf76e6298
Jason Woodward <jason@jwoodward.com>
2013-10-31 10:28:25 -0400

    trash files not supposed to be copied between instances, and some not allowed by wpengine git

--- wp-content/plugins/files/civicrm/templates_c/en_US/%%BB/BB4/BB46C32B%%CMSUser.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2013-10-29 19:52:46
-         compiled from CRM/Admin/Page/CMSUser.tpl */ ?>
-<?php $_smarty_tpl_vars = $this->_tpl_vars;
-$this->_smarty_include(array('smarty_include_tpl_file' => "CRM/Admin/Form/CMSUser.tpl", 'smarty_include_vars' => array()));
-$this->_tpl_vars = $_smarty_tpl_vars;
-unset($_smarty_tpl_vars);
- ?>
--- wp-content/plugins/files/civicrm/templates_c/en_US/%%F0/F0F/F0F71C33%%version.tpl.php
+++
@@ -1,3 +0,0 @@
-<?php /* Smarty version 2.6.27, created on 2013-10-29 18:35:38
-         compiled from CRM/common/version.tpl */ ?>
-4.3.5
