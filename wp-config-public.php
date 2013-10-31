6758fb98813eaeca9829d5cdd95d85a6c6e9fbc4
Jason Woodward <jason@jwoodward.com>
2013-10-31 17:20:15 -0400

    move extra wpengine config settings into wp-config

---
+++ wp-config-public.php
@@ -0,0 +1,158 @@
+<?php
+/**
+ * The base configurations of the WordPress.
+ *
+ * This file has the following configurations: MySQL settings, Table Prefix,
+ * Secret Keys, WordPress Language, and ABSPATH. You can find more information
+ * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
+ * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ *
+ * This file is used by the wp-config.php creation script during the
+ * installation. You don't have to use the web site, you can just copy this file
+ * to "wp-config.php" and fill in the values.
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wp_woodwardjd');
+
+/** MySQL database username */
+define('DB_USER', 'woodwardjd');
+
+/** MySQL database password */
+//define('DB_PASSWORD', '---');
+
+/** MySQL hostname */
+define('DB_HOST', '127.0.0.1');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', 'utf8_unicode_ci');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+// define('AUTH_KEY',         '---');
+// define('SECURE_AUTH_KEY',  '---');
+// define('LOGGED_IN_KEY',    '---');
+// define('NONCE_KEY',        '---');
+// define('AUTH_SALT',        '---');
+// define('SECURE_AUTH_SALT', '---');
+// define('LOGGED_IN_SALT',   '---');
+// define('NONCE_SALT',       '---');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each a unique
+ * prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_membership_';
+
+# Localized Language Stuff
+
+define('WP_CACHE',TRUE);
+
+define('PWP_NAME','woodwardjd');
+
+define('FS_METHOD','direct');
+
+define('FS_CHMOD_DIR',0775);
+
+define('FS_CHMOD_FILE',0664);
+
+define('PWP_ROOT_DIR','/nas/wp');
+
+define('WPE_APIKEY','a7d88ee515f9ba0ec3190efc4072ebe61dfc8573');
+
+define('WPE_FOOTER_HTML',"");
+
+define('WPE_CLUSTER_ID','1620');
+
+define('WPE_CLUSTER_TYPE','pod');
+
+define('WPE_ISP',true);
+
+define('WPE_BPOD',false);
+
+define('WPE_RO_FILESYSTEM',false);
+
+define('WPE_LARGEFS_BUCKET','largefs.wpengine');
+
+define('WPE_CDN_DISABLE_ALLOWED',true);
+
+define('DISALLOW_FILE_EDIT',FALSE);
+
+define('DISALLOW_FILE_MODS',FALSE);
+
+define('DISABLE_WP_CRON',false);
+
+define('WPE_FORCE_SSL_LOGIN',false);
+
+define('FORCE_SSL_LOGIN',false);
+
+/*SSLSTART*/ if ( isset($_SERVER['HTTP_X_WPE_SSL']) && $_SERVER['HTTP_X_WPE_SSL'] ) $_SERVER['HTTPS'] = 'on'; /*SSLEND*/
+
+define('WPE_EXTERNAL_URL',false);
+
+define('WP_POST_REVISIONS',FALSE);
+
+define('WPE_WHITELABEL','wpengine');
+
+define('WP_TURN_OFF_ADMIN_BAR',false);
+
+define('WPE_BETA_TESTER',false);
+
+umask(0002);
+
+$wpe_cdn_uris=array ();
+
+$wpe_no_cdn_uris=array ();
+
+$wpe_content_regexs=array ();
+
+$wpe_all_domains=array (  0 => 'woodwardjd.wpengine.com',);
+
+$wpe_varnish_servers=array (  0 => 'pod-1620',);
+
+$wpe_ec_servers=array ();
+
+$wpe_largefs=array ();
+
+$wpe_netdna_domains=array ();
+
+$wpe_netdna_push_domains=array ();
+
+$wpe_domain_mappings=array ();
+
+$memcached_servers=array ();
+define('WPLANG','');
+
+# WP Engine ID
+
+
+# WP Engine Settings
+
+
+
+
+
+
+# That's It. Pencils down
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
+
+$_wpe_preamble_path = null; if(false){}
