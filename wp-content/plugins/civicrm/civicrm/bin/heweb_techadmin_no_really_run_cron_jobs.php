72099a8cd59a10cc1a9e1d417584098fd08f8f8b
Jason Woodward <jason@jwoodward.com>
2013-12-06 19:02:06 -0500

    add job for running civicrm jobs without username/password/sitekey

---
+++ wp-content/plugins/civicrm/civicrm/bin/heweb_techadmin_no_really_run_cron_jobs.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once( dirname(__FILE__) . '../../civicrm.settings-private.php');
+
+$_REQUEST['name'] = HEWEB_TECHADMIN_USER; 
+$_REQUEST['pass'] = HEWEB_TECHADMIN_PASSWORD;
+$_REQUEST['key'] = CIVICRM_SITE_KEY; 
+$_GET['key'] = CIVICRM_SITE_KEY; 
+
+include( dirname(__FILE__) . '/cron.php');
+
+?>
