edbd7d326627954d09a1e80e4627120639bb2cc6
Jason Woodward <jason@jwoodward.com>
2013-12-13 00:26:40 -0500

    latest from curtiss over email

--- wp-content/mu-plugins/heweb-members-login-widget.php
+++ wp-content/mu-plugins/heweb-members-login-widget.php
@@ -7,3 +7,30 @@ function register_heweb_login_widget() {
 	require_once( plugin_dir_path( __FILE__ ) . 'heweb-members-login-widget/class-heweb-members-login-widget.php' );
 	register_widget( 'HEWeb_Members_Login_Widget' );
 }
+
+add_action( 'wp_enqueue_scripts', 'heweb_login_enqueue_jquery' );
+function heweb_login_enqueue_jquery() {
+	wp_enqueue_script( 'jquery' );
+}
+
+add_action( 'wp_print_footer_scripts', 'heweb_login_link_scripts' );
+function heweb_login_link_scripts() {
+	$url = $_SERVER['REQUEST_URI'];
+?>
+<script type="text/javascript">
+jQuery( function() {
+	jQuery( 'a[href*="wp-login.php"]' ).each( function() {
+		if ( jQuery( this ).attr( 'href' ).indexOf( 'redirect_to' ) < 0 ) {
+			var currenthref = jQuery( this ).attr( 'href' );
+			if ( currenthref.indexOf( '?' ) >= 0 ) {
+				currenthref = currenthref + '&redirect_to=<?php echo urlencode( $url ) ?>';
+			} else {
+				currenthref = currenthref + '?redirect_to=<?php echo urlencode( $url ) ?>';
+			}
+			jQuery( this ).attr( 'href', currenthref );
+		}
+	} );
+} );
+</script>
+<?php
+}
