9138ec247663ff73df1fbbf79b8f88f2fcfc68b8
Chad Killingsworth <chadkillingsworth@missouristate.edu>
2014-01-27 16:18:56 -0600

    Add framework for oauth membership query function.

--- wp-content/plugins/oauth-provider/oauth-provider.php
+++ wp-content/plugins/oauth-provider/oauth-provider.php
@@ -46,7 +46,29 @@ if (isset($_GET['oauth']) || isset($_POST['oauth'])) {
 }
 
 // Add Method Sample
-add_oauth_method('sayHello', create_function('$request, $userid, $username', 'return array("message" => "Hello {$username}!");'));
+//add_oauth_method('sayHello', create_function('$request, $userid, $username', 'return array("message" => "Hello {$username}!");'));
+
+
+function hewebmembershipinfo_oauth($request, $userid, $username) {
+	$member_email = $request->get_parameter('member_email');
+	// if user is not administrator
+	// 		return Array("message" => "not authorized");
+	
+	// Lookup membership info by email.
+	/*
+SELECT mem.id, display_name, first_name, last_name, job_title,
+organization_name, max(email.email) as email, memstatus.name as status, end_date
+FROM civicrm_contact as c
+INNER JOIN civicrm_membership as mem ON c.id = mem.contact_id
+INNER JOIN civicrm_membership_status as memstatus on mem.status_id = memstatus.id
+INNER JOIN civicrm_email as email on c.id = email.contact_id
+WHERE c.is_deleted = 0 and email.email = '$member_email'
+GROUP BY mem.id, display_name, first_name, last_name, job_title,
+organization_name, memstatus.name, end_date */
+
+	return array("message" => "Hello {$username}!");
+} 
+add_oauth_method('member_info', create_function('$request, $userid, $username', 'return array("message" => "Hello {$username}!");'));
 
 if (function_exists('register_activation_hook'))
 	register_activation_hook(__FILE__, 'flush_rewrite_rules');
