a7988de5deea7144eb69c4d259ed9cceb10c37da
Jason Woodward <jason@jwoodward.com>
2014-02-06 22:03:10 -0700

    no really, push what's on production

--- wp-content/plugins/heweb-wpe-extensions/heweb-wpe-extensions.php
+++ wp-content/plugins/heweb-wpe-extensions/heweb-wpe-extensions.php
@@ -10,31 +10,29 @@
  */
 
 function heweb_set_fswrite_cookie_if_necessary() {
-  error_log("heweb: scin start");
+  //error_log("heweb: scin start");
   if (heweb_needs_fswrite_cookie_set()) {
-    error_log("heweb: scin needs cookie set");
+    //error_log("heweb: scin needs cookie set");
     $wpe_cookie = 'wpe-auth';
     $cookie_value = md5('wpe_auth_salty_dog|'.WPE_APIKEY);
     if (!heweb_fswrite_cookie_set()) {
-      error_log("heweb: scin cookie not yet set");
+      //error_log("heweb: scin cookie not yet set");
       if (isset($_GET['heweb-fswrite'])) {
-        error_log("heweb: scin inside wp-admin, so setting and redirecting back to " . urldecode($_GET['backto']));
+        //error_log("heweb: scin inside wp-admin, so setting and redirecting back to " . urldecode($_GET['backto']));
         setcookie($wpe_cookie,$cookie_value,0,heweb_translate_request_uri_into_cookie_path());
         wp_redirect( urldecode($_GET['backto']), 302 );
         exit();
       } else {
-        // TODO: remove second assignment since the first works on production 
         $whereto = get_site_url( null, 'wp-login.php') . '?heweb-fswrite=true&backto=' . urlencode(heweb_selfURL());
-        //$whereto = 'http://hewmembership.staging.wpengine.com/wp-login.php' . '?heweb-fswrite=true&backto=' . urlencode(heweb_selfURL());
-        error_log("heweb: scin redirecting to where we know we can set a cookie: " . $whereto);
+        //error_log("heweb: scin redirecting to where we know we can set a cookie: " . $whereto);
         wp_redirect( $whereto, 302 );
         exit();
       }
     } else {
-      error_log("heweb: scin fswrite cookie already set");
+      //error_log("heweb: scin fswrite cookie already set");
       // refresh if necessary.  the following case came from code WPE support sent me.  I'm being slightly conservative here and only resetting it when we think they've cleared it
       if ( ! wp_get_current_user() || ! current_user_can('edit_pages') ) {
-        error_log("heweb: scin refreshing fswrite cookie");
+        //error_log("heweb: scin refreshing fswrite cookie");
         setcookie($wpe_cookie,$cookie_value,0,heweb_translate_request_uri_into_cookie_path());
       }
     }
